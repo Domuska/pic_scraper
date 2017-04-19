e4219648dd3097fcc3739630f2d3cad06a62c89e
Tomi Lämsä <lamsatom@gmail.com>
2017-04-19 15:49:55 +0300

    added error handling and counter that prints what was downloaded after script is done

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,16 +1,21 @@
 import praw, urllib, json, time, re
 import os.path
 
+#for web requests
 import requests
+
+#for exception logging purposes in the get_pics_by_subreddit
+import traceback
+import logging
 
 #imgur api https://api.imgur.com/oauth2
 #praw api http://praw.readthedocs.io/en/stable/index.html#main-page
 #gfycat api https://developers.gfycat.com/api/
 
 global imgur_client_id 
-imgur_client_id = '7d9521cca4c957e'
-gfycat_client_id = '2_s8KaAJ'
-gfycat_client_secret = 'c-DHAXrQ3RrUoBtKLxfkiL-grj8yMGEcymTP5ORZKiJK2Z9SMB1Surf3u9N9s3Yz'	
+imgur_client_id = 
+gfycat_client_id = 
+gfycat_client_secret = 
 
 def get_pics_by_subreddit(subreddit, limit):
 	user_agent = "windows:testing_agent:0.1 (by /u/Domuska)"
@@ -23,135 +28,167 @@ def get_pics_by_subreddit(subreddit, limit):
 	path = 'C:/scraper/' + subreddit
 	#variable that is used in file names
 	variable = 1
-	if not os.path.exists(path):
-		os.makedirs(path)
+	images_downloaded = 0
+	albums_downloaded = 0
+	videos_downloaded = 0
 	
-	for submission in submissions:
-		print ("\n")
-		print(submission.url)
-		url = submission.url
-		#use submission name as file name
-		title = submission.title
+	try:
+		if not os.path.exists(path):
+			os.makedirs(path)
 		
-		#http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
-		#remove reserved windows characters
-		title = slugify(title)
-		filename = title + "_" + str(submission.author) + "_" + str(variable)
-		
-		fullpath = os.path.join(path, filename)
-		#handle imgur separate from other image sources, imgur can have 
-		#albums and all that jazz
-		
-		if "imgur" in url:
-			download_from_imgur(url, fullpath)
+		for submission in submissions:
+			print ("\n")
+			print(submission.url)
+			url = submission.url
+			#use submission name as file name
+			title = submission.title
+			
+			#http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
+			#remove reserved windows characters
+			title = slugify(title)
+			#if reddit submission title only contains unallowed characters, add something as title
+			if len(title) == 0:
+				len = variable
+			filename = title + "_" + str(submission.author) + "_" + str(variable)
+			
+			fullpath = os.path.join(path, filename)
+			#handle imgur separate from other image sources, imgur can have 
+			#albums and all that jazz
+			
+			if "imgur" in url:
+				try:
+					download_from_imgur(url, fullpath)
+				except urllib.error.URLError as err:
+					print ("Error code: " + str(err.code))
+					print ("Reason: " + err.reason)
+					print ("Headers: " + err.headers)
+					
+					if err.code == 429:
+						print ("Stopping downloads, daily limit reached")
+						return 0
+					
+			
+			elif "reddituploads" in url or "tumblr" in url: 
+				if ".gif" in url:
+					path_with_extension = fullpath + ".gif"
+					save_image_with_url_path(url, path_with_extension)
+					images_downloaded += 1
+				else:
+					path_with_extension = fullpath + ".jpg"
+					save_image_with_url_path(url, path_with_extension)
+					images_downloaded += 1
+					
+					
+					
+			#https://gfycat.com/PessimisticReadyFrog
+			#todo: properly handle gfycat urls, read API and implement
+			#gfycat uses only https
+			#dirty fix, not nice, do something more reasonable, use API maybe to ask in which domain the video is?
+			elif "gfycat" in url:
+				#use a wizard's spell to reverse the url string
+				gfy_id = url[::-1]
+				print(gfy_id)
+				regex = re.search('/', gfy_id)
+				gfy_id = gfy_id[:regex.end()-1]
+				gfy_id = gfy_id[::-1]
+				print(gfy_id)
+				download_from_gfycat_with_id(gfy_id, fullpath)
 				
-		
-		elif "reddituploads" in url or "tumblr" in url: 
-			if ".gif" in url:
-				path_with_extension = fullpath + ".gif"
-				save_image_with_url_path(url, path_with_extension)
-			else:
-				path_with_extension = fullpath + ".jpg"
-				save_image_with_url_path(url, path_with_extension)
 				
 				
+				#if "https" not in url:
+				#	url = url[:4] + 's' + url[4:]
+				#if ".webm" in url:
+				#	path_with_extension = fullpath + ".webm"
+				#	print (gfycat_url)
+				#	save_image_with_url_path(gfycat_url, path_with_extension)
+				#elif ".mp4" in url:
+				#	path_with_extension = fullpath + ".mp4"
+				#	print (gfycat_url)
+				#	save_image_with_url_path(gfycat_url, path_with_extension)
+				#else:
+				#	#this try-catch is just for handling the zippy/giant/fat monstrosity
+				#	try:
+				#		path_with_extension = fullpath + ".webm"
+				#		#url = 'zippy.' + url + '.webm'
+				#		#direct urls to webms in gfycat have zippy/giant/fat and .webm extension in the url in these locations
+				#		gfycat_url = url[:8] + 'zippy.' + url[8:] + ".webm"
+				#		print (gfycat_url)
+				#		save_image_with_url_path(gfycat_url, path_with_extension)
+				#	except urllib.error.URLError as err:
+				#		try:
+				#			if err.code == 403:
+				#				gfycat_url = url[:8] + 'giant.' + url[8:] + ".webm"
+				#				print (gfycat_url)
+				#				save_image_with_url_path(gfycat_url, path_with_extension)
+				#		except urllib.error.URLError as err2:
+				#			try:
+				#				if err2.code == 403:
+				#					gfycat_url = url[:8] + 'fat.' + url[8:] + ".webm"
+				#					print (gfycat_url)
+				#					save_image_with_url_path(gfycat_url, path_with_extension)					
+				#			except urllib.error.URLError as err3:
+				#				if err3.code == 403:
+				#					print("Error 403, could not save video at url: " + gfycat_url)
+							
+			
+			variable += 1
 				
-		#https://gfycat.com/PessimisticReadyFrog
-		#todo: properly handle gfycat urls, read API and implement
-		#gfycat uses only https
-		#dirty fix, not nice, do something more reasonable, use API maybe to ask in which domain the video is?
-		elif "gfycat" in url:
-			#use a wizard's spell to reverse the url string
-			gfy_id = url[::-1]
-			print(gfy_id)
-			regex = re.search('/', gfy_id)
-			gfy_id = gfy_id[:regex.end()-1]
-			gfy_id = gfy_id[::-1]
-			print(gfy_id)
-			download_from_gfycat_with_id(gfy_id)
-			
-			
-			
-			#if "https" not in url:
-			#	url = url[:4] + 's' + url[4:]
-			#if ".webm" in url:
-			#	path_with_extension = fullpath + ".webm"
-			#	print (gfycat_url)
-			#	save_image_with_url_path(gfycat_url, path_with_extension)
-			#elif ".mp4" in url:
-			#	path_with_extension = fullpath + ".mp4"
-			#	print (gfycat_url)
-			#	save_image_with_url_path(gfycat_url, path_with_extension)
-			#else:
-			#	#this try-catch is just for handling the zippy/giant/fat monstrosity
-			#	try:
-			#		path_with_extension = fullpath + ".webm"
-			#		#url = 'zippy.' + url + '.webm'
-			#		#direct urls to webms in gfycat have zippy/giant/fat and .webm extension in the url in these locations
-			#		gfycat_url = url[:8] + 'zippy.' + url[8:] + ".webm"
-			#		print (gfycat_url)
-			#		save_image_with_url_path(gfycat_url, path_with_extension)
-			#	except urllib.error.URLError as err:
-			#		try:
-			#			if err.code == 403:
-			#				gfycat_url = url[:8] + 'giant.' + url[8:] + ".webm"
-			#				print (gfycat_url)
-			#				save_image_with_url_path(gfycat_url, path_with_extension)
-			#		except urllib.error.URLError as err2:
-			#			try:
-			#				if err2.code == 403:
-			#					gfycat_url = url[:8] + 'fat.' + url[8:] + ".webm"
-			#					print (gfycat_url)
-			#					save_image_with_url_path(gfycat_url, path_with_extension)					
-			#			except urllib.error.URLError as err3:
-			#				if err3.code == 403:
-			#					print("Error 403, could not save video at url: " + gfycat_url)
-						
-		
-		variable += 1
-			
-			
+		print("\n\nDownload finished!\n")
+		print("Images downloaded: " + images_downloaded + "\n")
+		print("Videos downloaded: " + videos_downloaded + "\n")
+		print("Albums downloaded: " + albums_downloaded)
+	
+	except Exception as e:
+		logging.error(traceback.format_exc())
 			
 def save_image_with_url_path(url, path):
 	print(urllib.request.urlretrieve(url, path))
 	
 #download an webm from gfycat with the gfycat ID supplied
 def download_from_gfycat_with_id(gfy_id, gfy_path = 'C:\scraper\gfycat_webms\\'):
-	
-	client_id = gfycat_client_id
-	token_request_url = 'https://api.gfycat.com/v1/oauth/token'	
-	
-	#requests kirjaston kikkailut
-	results = requests.get(token_request_url, params = {
-												'grant_type':'client_credentials',
-												'client_id':client_id,
-												'client_secret':gfycat_client_secret})
-	print("response from gfycat: " + results.text)
-	#result_dictionary = json.loads(results.json())
-	token = results.json()['access_token']
-	#token = results_dictionary['access_token']
-	print (token)
-	
-	#get info about the GFY requested
-	gfy_request_url = 'https://api.gfycat.com/v1/gfycats/'
-	gfy_request_url += gfy_id
-	results = requests.get(gfy_request_url, headers={
-												"Authorization": token})
-									
-	#get the URL and name of the GFY
-	print("gfy request response: " + results.text)
-	webm_url = results.json()['gfyItem']['webmUrl']
-	webm_name = results.json()['gfyItem']['gfyName'] + ".webm"
-	print ('webm url : ' + webm_url)
-	
-	gfy_path = os.path.realpath(gfy_path)
-	filepath = os.path.join(gfy_path, webm_name)
-	
-	if not os.path.exists(gfy_path):
-		os.makedirs(gfy_path)
-	
-	#save the GFY from the URL
-	urllib.request.urlretrieve(webm_url, filepath)
+	try:
+		client_id = gfycat_client_id
+		token_request_url = 'https://api.gfycat.com/v1/oauth/token'
+		
+		#requests kirjaston kikkailut
+		results = requests.get(token_request_url, params = {
+													'grant_type':'client_credentials',
+													'client_id':client_id,
+													'client_secret':gfycat_client_secret})
+		#print("response from gfycat: " + results.text)
+		#result_dictionary = json.loads(results.json())
+		token = results.json()['access_token']
+		#token = results_dictionary['access_token']
+		print (token)
+		
+		#get info about the GFY requested
+		gfy_request_url = 'https://api.gfycat.com/v1/gfycats/'
+		gfy_request_url += gfy_id
+		results = requests.get(gfy_request_url, headers={
+													"Authorization": token})
+										
+		#get the URL and name of the GFY
+		#print("gfy request response: " + results.text)
+		webm_url = results.json()['gfyItem']['webmUrl']
+		webm_name = results.json()['gfyItem']['gfyName'] + ".webm"
+		print ('webm url : ' + webm_url)
+		
+		gfy_path = os.path.realpath(gfy_path)
+		if not os.path.exists(gfy_path):
+			os.makedirs(gfy_path)
+		
+		gfy_path = gfy_path + "_" + webm_name
+		#filepath = os.path.join(gfy_path, webm_name)
+		
+		#save the GFY from the URL
+		print(urllib.request.urlretrieve(webm_url, gfy_path))
+		videos_downloaded += 1
+		
+	except urllib.error.URLError as err:
+		print ("Error code: " + str(err.code))
+		print ("Reason: " + err.reason)
+		print ("Headers: " + err.headers)
 	
 
 #download an arbitrary image or album from imgur
@@ -159,7 +196,7 @@ def download_from_gfycat_with_id(gfy_id, gfy_path = 'C:\scraper\gfycat_webms\\')
 #	path : the path to the image along with the file name (example, 'c:\pics\catpicture')
 #the file will be saved to the path provided, if the URL goes to an album _IMAGENUMBER will be
 #added after the file name
-def download_from_imgur(url, image_path = 'C:\scraper\imgur_album\\'):
+def download_from_imgur(url, image_path = 'C:\scraper\imgur_album\\', ):
 
 	#todo: repair this folder creation, now a folder is created for every pic that is created
 	if not os.path.exists(image_path):
@@ -178,6 +215,9 @@ def download_from_imgur(url, image_path = 'C:\scraper\imgur_album\\'):
 			path_with_extension = image_path +  "_" + str(album_variable) + ".jpg"
 			save_image_with_url_path(album_img_url, path_with_extension)
 			album_variable += 1
+			images_downloaded += 1
+			albums_downloaded += 1
+			
 					
 				
 	#convert imgur links to pure picture links			
@@ -192,12 +232,14 @@ def download_from_imgur(url, image_path = 'C:\scraper\imgur_album\\'):
 			#urllib.request.urlretrieve(url, fullpath + filename + ".mp4")
 			path_with_extension = image_path + ".mp4"
 			save_image_with_url_path(url, path_with_extension)
+			videos_downloaded += 1
 			
 		else:
 			url = url + ".jpg"
 			#urllib.request.urlretrieve(url, fullpath + filename + ".jpg")
 			path_with_extension = image_path + ".jpg"
 			save_image_with_url_path(url, path_with_extension)
+			images_downloaded += 1
 					
 
 #helper method to handle an imgur album
@@ -242,6 +284,8 @@ def handle_imgur_album(album_url):
 		print ("Reason: " + err.reason)
 		print ("Headers: " + err.headers)
 	
+	
+#returns: dictionary containing that has the info that the API provides of the picture
 def handle_imgur_picture(picture_url):
 	try:
 		#get the pic id with regexes
@@ -281,8 +325,8 @@ def handle_imgur_picture(picture_url):
 		
 	except urllib.error.URLError as err:
 		print ("Error code: " + str(err.code))
-		print ("Reason: " + err.reason)
-		print ("Headers: " + err.headers)
+		print ("Reason: " + str(err.reason))
+		print ("Headers: " + str(err.headers))
 		
 #http://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-filename-in-python
 #modified for python 3 from above answer
